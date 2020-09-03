f93ffee8adcf142d3b58af7b050d6b28b9de4e8b
Aaron Graham <aarongraham518@gmail.com>
2020-09-02 22:02:21 -0400

    Created AddLogModal.js along with using useState hooks and logic for component.  Tested state variables after submits of form fields.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Fragment, useEffect} from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
-
+import AddLogModal from './components/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -17,7 +17,8 @@ const App = () => {
     <Fragment>
     <SearchBar/>
       <div className="container">
-        <AddBtn/>        
+        <AddBtn/>   
+        <AddLogModal/>     
         <Logs/>
       </div>
       
